69001b48efd8dff81743a2197e84bf662a4e6639
TRIN TRIN <136374536+TRINTRIN01@users.noreply.github.com>
2023-06-16 11:52:18 -0300

    Database Atualizado 16/06

--- DATABASE_TRINTRIN.sql
+++ DATABASE_TRINTRIN.sql
@@ -1,3 +1,4 @@
+create database trintrin;
 use trintrin;
 
 create table Locador(
@@ -25,4 +26,12 @@ email varchar(130),
 senha varchar(16)
 );
 
-select * from usuario;
+create table Bicicleta(
+	Bicicleta_id int primary key not null auto_increment,
+    nomeBicicleta varchar(150) not null,
+    nomeLocador varchar(150) not null,
+    valor float(0.2),
+    telefone char(15) not null
+);
+
+select * from Bicicleta;
